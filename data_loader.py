554cdfd4429c72f1f209fec887600c2ddeba8f72
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-10-21 17:29:58 +0100

    added script for loading MNIST data

---
+++ data_loader.py
@@ -0,0 +1,20 @@
+"""module to load image data using tensorflow"""
+
+from typing import Tuple
+from tensorflow.keras import datasets  # pyright: ignore[reportMissingImports]
+
+def load_image_data() -> Tuple:
+    """method to load MNIST images data"""
+
+    # Loading the data
+    (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
+
+    # Normalizing the pixel values to [0,1]
+    X_train, X_test = X_train/255.0, X_test/255.0
+
+    # Reshape data to include channel dimension (28, 28, 1)
+    X_train = X_train.reshape((X_train.shape[0], 28, 28, 1))
+    X_test = X_test.reshape((X_test.shape[0], 28, 28, 1))
+
+    return (X_train, y_train), (X_test, y_test)
+
