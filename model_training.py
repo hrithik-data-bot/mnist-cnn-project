03bdefe1799dff1e0cf9f5becb1f529cc8933bf0
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-10-21 20:39:10 +0100

    working code

--- model_training.py
+++ model_training.py
@@ -13,27 +13,50 @@ class CNNModel:
     def __init__(self, x_train: np.ndarray, y_train: np.uint8) -> None:
         self.x_train = x_train
         self.y_train = y_train
+        self.input_shape = x_train.shape[1:]
+        self.num_classes = len(np.unique(y_train))
 
-
-    def model(self) -> models.Sequential:
-        """model method return model"""
+    def cnn_model(self) -> models.Sequential:
+        """method to build, train, and return the model's training history"""
 
         model = models.Sequential([
-                                    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-                                    layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(32, (3, 3), activation='relu', input_shape=self.input_shape),
+            layers.MaxPooling2D((2, 2)),
 
-                                    layers.Conv2D(64, (3, 3), activation='relu'),
-                                    layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
 
-                                    layers.Flatten(),
-                                    layers.Dense(128, activation='relu'),
-                                    layers.Dropout(0.4),
-                                    layers.Dense(10, activation='softmax')
-                                ])
-        return model
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dropout(0.4),
+            layers.Dense(self.num_classes, activation='softmax')  # dynamic number of classes
+        ])
+
+        model.compile(
+            optimizer='adam',
+            loss='sparse_categorical_crossentropy',
+            metrics=['accuracy']
+        )
+
+        # Display model summary
+        print(f"{'*'*20} Model Summary {'*'*20}")
+        print(model.summary())
+        print("\n")
+        print(f"{'*'*20} Model Training Started {'*'*20}")
+
+        history = model.fit(
+            self.x_train,
+            self.y_train,
+            epochs=5,
+            batch_size=64,
+            validation_split=0.1
+        )
+
+        print(f"{'*'*20} Model Training Ended {'*'*20}")
+        return history
 
 
-    def train(self):
-        """train method for model"""
+if __name__ == "__main__":
+    m = CNNModel(X_train, y_train)
+    m.cnn_model()
 
-        pass
